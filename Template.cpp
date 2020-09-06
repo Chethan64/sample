1f3bca7fecb6990027ea75108b6a57cee6e8c385
Chethan64 <padma.chethan@gmail.com>
2020-09-06 23:15:53 +0530

    Added template for cpp program.

---
+++ Template.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define FIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+
+void solve()
+{
+
+}
+
+int main()
+{
+    FIO;
+    ll T;
+    cin>>T;
+    while(T--)
+    {
+        solve();
+    }
+    return 0;
+}
